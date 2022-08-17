16d727084c3f42b61a1b466cd63e32e1742aaa05
Keknokz <gardennathan07@gmail.com>
2022-08-17 20:29:46 +0100

    added more comments

--- joke_picker.py
+++ joke_picker.py
@@ -12,15 +12,18 @@ def rand_joke():
     num_jokes - 1
     # this is how many jokes are in the list
     
-    joke_titles = list(jokes.keys())
+    joke_titles = list(jokes.keys()) 
+    # this makes it so its not saved as a dict object
     joke_bodys = []
     
     for x in jokes.keys():
         joke_bodys.append(jokes[x])
     
     rand_index = random.randint(0, num_jokes)
+    # this pick a random number
     
     joke_leadup = joke_titles[rand_index]
     joke_punch = joke_bodys[rand_index]
+    # this picks a random joke from the lists and returns them
       
     return joke_leadup, joke_punch 
